9418643793395a27fd4ee1e49c9eef00e190dee4
rusnnee <rusne.jon@gmail.com>
2018-02-18 13:11:08 +0200

    image

--- web/models.py
+++ web/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     section = models.CharField(max_length=100)
-    # image = models.ImageField(upload_to='Student')
+    image = models.ImageField(upload_to='student', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- web/views.py
+++ web/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from web.models import Student
+
+
 def index(request):
-    return render(request, 'index.html')
+    students = Student.objects.all()
+
+    return render(request, 'index.html', {'students': students})
