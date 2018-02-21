d98b4e79f34a179111d49506244daac6bf339912
rusnnee <rusne.jon@gmail.com>
2018-02-21 11:31:03 +0200

    tikrinimas

--- web/views.py
+++ web/views.py
@@ -7,7 +7,6 @@ from web.models import Student
 from web.models import Question
 
 
-
 def index(request):
     students = Student.objects.all()
     questions = Question.objects.all()
@@ -17,8 +16,19 @@ def index(request):
         form = AnswersForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            cleaned_data = request.POST.getlist('answer')
-            print(cleaned_data)
+            user_answers = request.POST.getlist('answer')
+            user_questions = request.POST.getlist('question')
+
+            questions_with_answers = []
+            for (pk, ans) in zip(user_questions, user_answers):
+                question = Question.objects.filter(pk=pk).first()
+                questions_with_answers.append((question, ans))
 
 
-    return render(request, 'index.html', {'students': students, 'answersform': answersform, 'questions':questions})
+            # questions_with_answers = zip(user_questions, user_answers)
+            return render(request, 'index.html',
+                          {'students': students, 'answersform': answersform, 'questions': questions,
+                           'questions_with_answers': questions_with_answers})
+
+
+    return render(request, 'index.html', {'students': students, 'answersform': answersform, 'questions': questions})
